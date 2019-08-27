94fa9b951d56a3bf7824ac191318b0501608b7c8
Ziv <zivalt8@gmail.com>
2019-08-27 22:05:21 +0300

    fixing resolve conflicts(use_effect)

--- backend/api.py
+++ backend/api.py
@@ -30,11 +30,12 @@ def board_output():
     return jsonify(output_json)
 
 
-@app.route('/restart', methods=['POST'])
+@app.route('/restart', methods=['GET'])
 def restart():
     board.rest()
     board.create_players()
     turn.set_player("red")
+    return ""
 
 
 @app.route('/pick', methods=['POST'])
